140a1a34eeec1b1b86d0425242cb4c6c94de4bf6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-11 09:59:00 +0000

    Deploy designer.krakend.io to github.com/devopsfaith/krakendesigner.git:gh-pages

--- wasm/main.go
+++ wasm/main.go
@@ -5,19 +5,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
-	"strings"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"syscall/js"
 
 	"github.com/devopsfaith/krakend/config"
 	"github.com/devopsfaith/krakend/logging"
 	"github.com/devopsfaith/krakend/proxy"
-	"github.com/devopsfaith/krakend/transport/http/client"
+	krakendgin "github.com/devopsfaith/krakend/router/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func parse(i []js.Value) {
-	println("parsing")
-
 	if len(i) < 2 {
 		println("not enough args")
 		return
@@ -29,7 +31,7 @@ func parse(i []js.Value) {
 	}
 
 	logger := func(msg string) {
-		print(msg)
+		fmt.Println(msg)
 	}
 	if len(i) > 2 {
 		if i[2].Type() == js.TypeFunction {
@@ -39,119 +41,154 @@ func parse(i []js.Value) {
 		}
 	}
 
-	cfg, err := config.NewParserWithFileReader(func(s string) ([]byte, error) {
-		return []byte(s), nil
-	}).Parse(i[0].String())
+	client, err := newJSClient(i[0].String(), logger)
 	if err != nil {
 		logger(err.Error())
 		return
 	}
 
-	k := JS{
+	i[1].Invoke(client.Value())
+}
+
+func newJSClient(cfg string, logger func(string)) (*JSClient, error) {
+	server, err := newServer(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &JSClient{
+		client: &Client{server},
 		logger: logger,
-		c: Config{
-			ServiceConfig: cfg,
-			res:           make(chan *proxy.Response, 1),
-			err:           make(chan error, 1),
-		},
-	}
-
-	i[1].Invoke(js.NewCallback(k.testEndpoint))
+	}, nil
 }
 
-type JS struct {
-	c      Config
-	logger func(msg string)
+type JSClient struct {
+	client *Client
+	logger func(string)
 }
 
-func (j JS) testEndpoint(i []js.Value) {
-	if len(i) < 5 {
-		j.logger("not enough args")
-		return
-	}
+func (j *JSClient) Value() js.Value {
+	opt := js.Global().Get("Object").New()
 
-	if i[4].Type() != js.TypeFunction {
-		j.logger("arg 4 should be a function")
-		return
-	}
+	opt.Set("close", js.NewCallback(func(_ []js.Value) { j.client.Close() }))
 
-	endpoint := i[0].Int()
-	params := map[string]string{}
-	if err := json.Unmarshal([]byte(i[1].String()), &params); err != nil {
-		j.logger(err.Error())
-		return
-	}
-
-	headers := map[string][]string{}
-	if err := json.Unmarshal([]byte(i[2].String()), &headers); err != nil {
-		j.logger(err.Error())
-		return
-	}
-
-	body := i[3].String()
-
-	j.c.testEndpoint(endpoint, params, headers, body)
-	go func() {
-		select {
-		case err := <-j.c.err:
+	opt.Set("test", js.NewCallback(func(i []js.Value) {
+		if len(i) < 5 {
+			j.logger("the test function requires at least 5 arguments: method, path, body, headers and a callback")
+			return
+		}
+		var reqBody io.Reader
+		if b := i[2].String(); b != "" {
+			reqBody = bytes.NewBufferString(b)
+		}
+		req, err := http.NewRequest(i[0].String(), i[1].String(), reqBody)
+		if err != nil {
 			j.logger(err.Error())
 			return
-		case res := <-j.c.res:
-			body, _ := json.MarshalIndent(res.Data, "", "\t")
-			headers, _ := json.MarshalIndent(res.Metadata.Headers, "", "\t")
-
-			i[4].Invoke(string(body), res.IsComplete, res.Metadata.StatusCode, string(headers))
 		}
-	}()
-}
-
-type Config struct {
-	config.ServiceConfig
-	res chan *proxy.Response
-	err chan error
-}
-
-func (c Config) testEndpoint(endpoint int, params map[string]string, headers map[string][]string, body string) {
-	if endpoint < 0 || len(c.Endpoints) < endpoint {
-		c.err <- fmt.Errorf("unknown endpoint %d", endpoint)
-		return
-	}
-	buf := new(bytes.Buffer)
-	logger, _ := logging.NewLogger("DEBUG", buf, "[KRAKEND]")
-	p, err := proxy.NewDefaultFactory(proxy.CustomHTTPProxyFactory(client.NewHTTPClient), logger).New(c.Endpoints[endpoint])
-	if err != nil {
-		c.err <- err
-		return
-	}
-	cleanParams := make(map[string]string, len(params))
-	for k, v := range params {
-		cleanParams[strings.Title(k)] = v
-	}
-
-	req := &proxy.Request{
-		Params:  cleanParams,
-		Method:  c.Endpoints[endpoint].Method,
-		Headers: headers,
-	}
-	if len(body) > 0 {
-		req.Body = ioutil.NopCloser(bytes.NewBufferString(body))
-	}
-
-	go func(name string, p proxy.Proxy, buf *bytes.Buffer, req *proxy.Request) {
-		resp, err := p(context.Background(), req)
-		if err != nil {
-			c.err <- err
+		headers := map[string][]string{}
+		if err := json.Unmarshal([]byte(i[3].String()), &headers); err != nil {
+			j.logger(err.Error())
 			return
 		}
-		c.res <- resp
-	}(c.Endpoints[endpoint].Endpoint, p, buf, req)
+		req.Header = headers
 
+		go func() {
+			resp := j.client.Do(req)
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				j.logger(err.Error())
+				return
+			}
+			resp.Body.Close()
+
+			headers := js.Global().Get("Object").New()
+			for k, v := range resp.Header {
+				headers.Set(k, v[0])
+			}
+
+			jsResp := js.Global().Get("Object").New()
+			jsResp.Set("statusCode", resp.StatusCode)
+			jsResp.Set("header", headers)
+			jsResp.Set("body", string(body))
+
+			i[4].Invoke(jsResp)
+		}()
+	}))
+
+	return opt
+}
+
+type Client struct {
+	Server *LocalServer
+}
+
+func (c Client) Do(req *http.Request) *http.Response {
+	// req.Header.Add("js.fetch:mode", "no-cors") // -> will trigger the CORB protection
+	req.Header.Add("js.fetch:credentials", "omit")
+	rw := httptest.NewRecorder()
+	c.Server.handler(rw, req)
+	return rw.Result()
+}
+
+func (c *Client) Close() {
+	c.Server.close()
+}
+
+type LocalServer struct {
+	close   func()
+	handler func(rw http.ResponseWriter, req *http.Request)
+}
+
+func newServer(cfg string) (*LocalServer, error) {
+	serviceConfig, err := config.NewParserWithFileReader(func(s string) ([]byte, error) {
+		return []byte(s), nil
+	}).Parse(cfg)
+	if err != nil {
+		return nil, err
+	}
+	serviceConfig.Debug = true
+
+	logger, err := logging.NewLogger("DEBUG", os.Stdout, "[KRAKEND]")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, e := range serviceConfig.Endpoints {
+		e.HeadersToPass = append(e.HeadersToPass, "js.fetch:mode", "js.fetch:credentials")
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	s := &LocalServer{
+		close:   cancel,
+		handler: func(rw http.ResponseWriter, req *http.Request) {},
+	}
+
+	routerFactory := krakendgin.NewFactory(krakendgin.Config{
+		Engine:         gin.New(),
+		ProxyFactory:   proxy.DefaultFactory(logger),
+		Logger:         logger,
+		HandlerFactory: krakendgin.EndpointHandler,
+		RunServer: func(ctx context.Context, _ config.ServiceConfig, handler http.Handler) error {
+			s.handler = handler.ServeHTTP
+			<-ctx.Done()
+			return ctx.Err()
+		},
+	})
+
+	go func(ctx context.Context, serviceConfig config.ServiceConfig) {
+		routerFactory.NewWithContext(ctx).Run(serviceConfig)
+	}(ctx, serviceConfig)
+
+	return s, nil
 }
 
 func main() {
-	println("WASM Go Initialized")
+	fmt.Println("WASM Go Initialized")
 
 	js.Global().Set("parse", js.NewCallback(parse))
+	js.Global().Get("onKrakendClientReady").Invoke()
 
 	select {}
 }
